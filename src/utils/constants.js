61b20e85fcc4931b3e36662365a57942472c727f
Omkar <omkard2702@gmail.com>
2026-06-03 04:55:05 +0530

    Update API URL for production deployment

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://127.0.0.1:8000/api';
+export const API_BASE_URL = 'https://expense-budget-tracker-api.onrender.com/api';
 
 export const ROLES = {
   ADMIN: 'Admin',
